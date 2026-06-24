daeb738887769f09bbc0f4dbeb41115a2352ac06
Junaid Sayyed <junaidsayyed778@gmail.com>
2026-06-24 19:45:53 +0530

    add components in frontend

---
+++ access-token/auth-artokens/src/routes/AppRoutes.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MainLayout from "../layouts/mainLayout";
+import Home from "../pages/Home";
+
+const AppRoutes = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AuthLayout />,
+      children: [
+        {
+            path: "",
+            element: <Login/>
+        },
+        {
+            path: "",
+            element: <Register/>
+        }
+      ],
+    },
+    {
+        path: "/home",
+        element: <MainLayout/>,
+        children: [
+            {
+                path: "",
+                element: <Home/>
+            }
+        ]
+    }
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default AppRoutes;
