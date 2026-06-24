249e5668e117c9536f794c1b245a1b587bccf9cb
Junaid Sayyed <junaidsayyed778@gmail.com>
2026-06-24 15:34:33 +0530

    add frontend

--- access-token/AR-tokens/src/app.js
+++ access-token/AR-tokens/src/app.js
@@ -2,11 +2,19 @@ const cookieParser = require("cookie-parser");
 const express = require("express");
 const authRoutes = require("./routes/authRoute");
 const morgan = require("morgan");
+const cors = require("cors")
 const app = express();
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        Credential: true,
+    }),
+);
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
 app.use("/api/auth", authRoutes);
 
