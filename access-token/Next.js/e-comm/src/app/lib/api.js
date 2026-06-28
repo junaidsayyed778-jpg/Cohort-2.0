38a8712ce541cf9b50acba75c6c4292c8f986fa3
Junaid Sayyed <junaidsayyed778@gmail.com>
2026-06-28 18:22:37 +0530

    add typescript

--- access-token/Next.js/e-comm/src/app/lib/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export let api = axios.create({
-  baseUrl: "https://api.team-sync.space",
-  withCredentials: true,
-});
--- access-token/Next.js/e-comm/src/app/login/page.js
+++ access-token/Next.js/e-comm/src/app/login/page.js
@@ -1,28 +1,55 @@
+"use client";
 import Link from "next/link";
 
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { api } from "../../lib/api";
 
 export default function LoginPage() {
+  const [formData, setFormData] = useState({});
+
+
+  let handleChange = (e) => {
+    let { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  let handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try{
+      let res = await api.post("/api/auth/login", formData);
+      console.log(res)
+    }catch(error) {
+      console.log("error in login", error)
+      console.log(error.response?.status);
+  console.log(error.response?.data);
+    }
+  }
   return (
     <div className="flex min-h-screen items-center justify-center bg-muted/30 px-4">
       <Card className="w-full max-w-md">
         <CardHeader className="space-y-2 text-center">
-          <CardTitle className="text-3xl font-bold">
-            Welcome Back
-          </CardTitle>
-          <CardDescription>
-            Sign in to continue to your account
-          </CardDescription>
+          <CardTitle className="text-3xl font-bold">Welcome Back</CardTitle>
+          <CardDescription>Sign in to continue to your account</CardDescription>
         </CardHeader>
 
         <CardContent>
-          <form className="space-y-5">
+          <form onSubmit={handleSubmit} className="space-y-5">
             <div className="space-y-2">
               <Label htmlFor="email">Email</Label>
               <Input
+                name="email"
+                onChange={handleChange}
                 id="email"
                 type="email"
                 placeholder="john@example.com"
@@ -32,15 +59,15 @@ export default function LoginPage() {
             <div className="space-y-2">
               <Label htmlFor="password">Password</Label>
               <Input
+                name={"password"}
+                onChange={handleChange}
                 id="password"
                 type="password"
                 placeholder="••••••••"
               />
             </div>
 
-            <Button className="w-full">
-              Login
-            </Button>
+            <Button className="w-full">Login</Button>
           </form>
 
           <p className="mt-6 text-center text-sm text-muted-foreground">
